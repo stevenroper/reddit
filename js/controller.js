fddde0cd8180b91cce6968631f6a00c2e08486ee
Steven Roper <steven.m.roper@gmail.com>
2015-02-18 19:23:43 -0700

    Finishing Step 3 (getting data from Firebase).

--- js/controller.js
+++ js/controller.js
@@ -2,4 +2,11 @@ var app = angular.module('reddit');
 
 app.controller('PostsController', function($scope, FirebaseService) {
 
+	$scope.getPosts = function() {
+		FirebaseService.getData().then(function(posts) {
+			$scope.posts = posts;
+		});
+	};
+
+	$scope.getPosts();
 });
--- js/service.js
+++ js/service.js
@@ -3,10 +3,17 @@ var app = angular.module('reddit');
 app.service('FirebaseService', function($http, $q) {
 
 	this.getData = function() {
+		var deferred = $q.defer();
 		$http({
 			method: 'GET',
 			url: 'https://devmtn.firebaseio.com/posts.json'
+		}).then(function onSuccess(data) {
+			deferred.resolve(data.data);
+		}, function onFailure(reason) {
+			deferred.reject(reason);
 		});
+
+		return deferred.promise;
 	};
 
 });
