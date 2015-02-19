b0f86838496978e97f1695de8ea8e5c310e5c003
Steven Roper <steven.m.roper@gmail.com>
2015-02-18 19:16:43 -0700

    Finishing Step 2.

--- js/service.js
+++ js/service.js
@@ -2,4 +2,11 @@ var app = angular.module('reddit');
 
 app.service('FirebaseService', function($http, $q) {
 
+	this.getData = function() {
+		$http({
+			method: 'GET',
+			url: 'https://devmtn.firebaseio.com/posts.json'
+		});
+	};
+
 });
